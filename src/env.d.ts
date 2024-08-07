d469da61dcfd74bed87b9fdd26d7ccd7234356fb
rmdxdev <rmdxdev@gmail.com>
2024-08-07 19:07:34 +0500

    feat: update types for env

--- src/env.d.ts
+++ src/env.d.ts
@@ -2,15 +2,15 @@ type Env = 'production' | 'development'
 
 declare namespace NodeJS {
   interface ProcessEnv {
-    JWT_ACCESS_TOKEN: string
-    JWT_REFRESH_TOKEN: string
-    BCRYPT_SALT: string
-    API_PORT: string
-    SMTP_SERVICE: string
-    SMTP_PORT: string
-    SMTP_EMAIL: string
-    SMTP_PASSWORD: string
-    FRONTEND_URL: string
-    NODE_ENV: Env
+    readonly JWT_ACCESS_TOKEN: string
+    readonly JWT_REFRESH_TOKEN: string
+    readonly BCRYPT_SALT: string
+    readonly API_PORT: string
+    readonly SMTP_SERVICE: string
+    readonly SMTP_PORT: string
+    readonly SMTP_EMAIL: string
+    readonly SMTP_PASSWORD: string
+    readonly FRONTEND_URL: string
+    readonly NODE_ENV: Env
   }
 }
