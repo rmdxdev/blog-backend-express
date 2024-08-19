f1907d6f63ddd9f610c46c79a66dd320784594e9
rmdxdev <rmdxdev@gmail.com>
2024-08-19 15:28:59 +0500

    feat: update constants

--- src/constants.ts
+++ src/constants.ts
@@ -6,8 +6,9 @@ export const JWT_ACCESS_TOKEN_SECRET = process.env.JWT_ACCESS_TOKEN
 export const JWT_REFRESH_TOKEN_SECRET = process.env.JWT_REFRESH_TOKEN
 
 export const BCRYPT_SALT = process.env.BCRYPT_SALT
-export const CLIENT_URL = process.env.FRONTEND_URL
 
 export const DEFAULT_LANG = 'ru'
 
 export const API_PORT = process.env.API_PORT
+
+export const CLIENT_URL = process.env.FRONTEND_URL
