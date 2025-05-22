084e79b081206aa294f1fce06eec72579edc7395
rmdxdev <rmdxdev@gmail.com>
2025-05-22 17:13:25 +0500

    feat: add rate limit

--- src/helpers/app/register-middlewares.helper.ts
+++ src/helpers/app/register-middlewares.helper.ts
@@ -3,9 +3,21 @@ import compression from 'compression'
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
 import express, { Express, Router } from 'express'
+import rateLimit from 'express-rate-limit'
 import helmet from 'helmet'
 import hpp from 'hpp'
+import { ReasonPhrases, StatusCodes } from 'http-status-codes'
 import { constants } from 'zlib'
+
+const limiter = rateLimit({
+  limit: 50,
+  windowMs: 1000,
+  handler: (_, res) => {
+    res.status(StatusCodes.TOO_MANY_REQUESTS).json({
+      message: ReasonPhrases.TOO_MANY_REQUESTS
+    })
+  }
+})
 
 export const registerMiddlewares = (app: Express, router: Router) => {
   app.use(helmet())
@@ -15,6 +27,7 @@ export const registerMiddlewares = (app: Express, router: Router) => {
   app.use(cookieParser())
   app.use(express.json())
 
+  app.use('/api', limiter)
   app.use('/api', router)
   app.use('/images', express.static('images'))
 }
